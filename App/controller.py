13ccc39a72f77764813422fe062a96e2b95e4e62
UsuarioGithub <correo@github.com>
2023-08-15 23:51:23 -0500

    modificaciones individuales laboratorio 1

--- App/controller.py
+++ App/controller.py
@@ -85,7 +85,7 @@ def loadBooksTags(control, filename):
     """
     Carga los tags de los libros del archivo y los agrega a la lista de tags. Siga el mismo procedimiento que en la carga de libros. """
     catalog = control.get("model")
-        booksfile = os.path.join(cf.data_dir, filename)
+    booksfile = os.path.join(cf.data_dir, filename)
     catalog = model.addBookTags(catalog, booksfile)
     if model.emptyBookTags(catalog):
         return None 
